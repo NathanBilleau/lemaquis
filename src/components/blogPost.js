d17b9a01a53048110d6dc3f2dd9286e665a2a079
Nathan Billeau <nbilleau@gmail.com>
2019-03-11 17:57:47 +0100

    style blogpost component

--- src/components/blogPost.js
+++ src/components/blogPost.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,15 +8,17 @@ import HomeEvent from "../components/homeEvent"
 import "../styles/blogPost.scss"
 
 const BlogPage = () => (
-    <article>
-        <div className="thumbnail" style={{backgroundImage:'url(https://images.unsplash.com/photo-1552084007-76f5feb8d22a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80)'}}></div>
-        <div className="infos">
-            <h1>succès pour“Corps et sensation”</h1>
-            <h2>Exposition - </h2>
-            <h3>8 mars</h3>            
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-    </article>
+	<Link to="/blog">
+		<article>
+	        <div className="thumbnail" style={{backgroundImage:'url(https://images.unsplash.com/photo-1552084007-76f5feb8d22a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80)'}}></div>
+	        <div className="infos">
+	            <h1>succès pour“Corps et sensation”</h1>
+	            <h2>Exposition - <span>8 mars</span></h2>          
+	            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+	        </div>
+	    </article>
+	</Link>
+    
 )
 
 export default BlogPage
--- src/pages/search.js
+++ src/pages/search.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import HomeEvent from "../components/homeEvent"
 
 const IndexPage = () => (
   <Layout>
