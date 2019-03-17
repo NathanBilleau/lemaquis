b0e17f7c511c1756b66df92f47c2560e2426ee5d
Nathan Billeau <nbilleau@gmail.com>
2019-03-17 17:41:32 +0100

    events

--- src/templates/evenement.js
+++ src/templates/evenement.js
@@ -14,7 +14,7 @@ export default function EvenementPage ({data})  {
 
   return (
     <Layout>
-      <SEO title={"succès pour “Corps et sensation”"} keywords={[`gatsby`, `application`, `react`]} />
+      <SEO title={event.frontmatter.title} keywords={[`gatsby`, `application`, `react`]} />
       <div className="Evenement">
       
         <div className="content">
@@ -32,7 +32,7 @@ export default function EvenementPage ({data})  {
 
           <div className="map" style={{overflow:"hidden", maxWidth:"100%", width:500+"px", height:500+'px'}}>
             <div id="googlemaps-display" style={{height: "100%", width: "100%", maxWidth: "100%"}}>
-              <iframe key="map" style={{height: "100%", width: "100%", border:"0"}} frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Place+de+la+république,+Le+Mans&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
+              <iframe title="map" style={{height: "100%", width: "100%", border:"0"}} frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Place+de+la+république,+Le+Mans&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
             </div>          
           </div>
         </div>             
