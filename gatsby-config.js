97e3bd7fe8845c66141b25969733c29e7b11a4b6
Nathan Billeau <nbilleau@gmail.com>
2019-03-20 18:28:01 +0100

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,13 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-136585940-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
