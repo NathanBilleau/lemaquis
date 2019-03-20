492e0af24ae12516224abb4a8c0f6ec65f5d4169
Nathan Billeau <nbilleau@gmail.com>
2019-03-20 21:42:06 +0100

    config cms

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
@@ -6,67 +7,110 @@ import Header from "./header"
 import Menu from "./menu"
 
 import { MenuContext } from './context'
- 
-class Layout extends React.Component {
 
-  constructor (props) {
-    super(props)
-    
+
+class Layout extends React.Component { 
+   constructor( props, {data}) {
+    super (props)
     this.state = {
       active: false,
       search: false,
       toggleMenu: this.toggleMenu
     }
-  }
+   }
 
-  toggleMenu = () => {
-   this.setState({
-    active: !this.state.active
-   })
- }
+    toggleMenu = () => {
+     this.setState({
+      active: !this.state.active
+     })
 
- toggleSearch = () => {
-   this.setState({
-     search: !this.state.search
-   })
- }
+     return null
+   }
+
+   toggleSearch = () => {
+     this.setState({
+       search: !this.state.search
+     })
+
+     return null
+   }
 
 
-  render () {
+    render() {
+
+
     return (
       <MenuContext.Provider value={this.state}>
-      <div>
-          <Header />
-          <Menu />
+        <div>
+            <Header />
+            <Menu />
 
-          <div>
-            <aside>
-              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/le_maquis_art/">
-                instagram
-              </a>
-                  
-              <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/lemaquislemans">
-                twitter
-              </a>
-            </aside>
-          <main>{this.props.children}</main>
+            <div>
 
-          <footer>
-            <Link to="/">Le Maquis</Link> - <Link to="/politique-de-confidentialite">Politique de confidentialité</Link> - Site réalisé par Nathan Billeau
-          </footer>
+            <StaticQuery
+              query={graphql`
+                {
+                  markdownRemark (frontmatter: {title: {eq: "Le Maquis"}}){
+                    id
+                    frontmatter {
+                      title
+                      twitter
+                      instagram
+                    }
+                  }
+                }
+              `}
+              render={data => (
+                  <aside>
+                    <a target="_blank" rel="noopener noreferrer" href={data.markdownRemark.frontmatter.instagram}>
+                      instagram
+                    </a>
+                        
+                    <a target="_blank" rel="noopener noreferrer" href={data.markdownRemark.frontmatter.twitter}>
+                      twitter
+                    </a>
+                  </aside>
+                  )}
 
-          </div>
-      </div> 
+               />
+              
+            <main>{this.props.children}</main>
+
+            <footer>
+              <Link to="/">Le Maquis</Link> - <Link to="/politique-de-confidentialite">Politique de confidentialité</Link> - Site réalisé par Nathan Billeau
+            </footer>
+
+            </div>
+        </div> 
       </MenuContext.Provider>
     )
-  }
-
+  }  
 }
-
+ 
 
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
+
+export const query = graphql`
+{
+  allMarkdownRemark (filter: {frontmatter: {title: {eq: "Le Maquis"}}}) {
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          twitter
+          instagram
+        }
+      }
+    }
+  }
+}
+`
+
 export default Layout
+
+
